1616bb06ff80b6a0433bf69ea23ef570c8c03ed1
Kerem Goksel <bkgoksel@gmail.com>
2023-09-24 10:49:51 -0700

    no more netlify

--- index.ts
+++ index.ts
@@ -4,11 +4,8 @@ import OpenAI from "openai";
 // Initialize the OpenAI client
 const openai = new OpenAI();
 
-let REQUEST_TIMES: number[] = [];
-const RATE_LIMIT = 10;  // Requests per minute
-
-
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    console.log(`Got new ${event.httpMethod} request`)
     const current_time = Date.now();
     const headers = {
         "Access-Control-Allow-Origin": "https://bkgoksel.github.io",
@@ -33,17 +30,6 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
         };
     }
 
-    // Rate limiting
-    REQUEST_TIMES = REQUEST_TIMES.filter(t => current_time - t < 60 * 1000);
-    if (REQUEST_TIMES.length >= RATE_LIMIT) {
-        return {
-            statusCode: 429,
-            body: JSON.stringify({ error: "Rate limit exceeded" }),
-            headers: headers
-        };
-    }
-    REQUEST_TIMES.push(current_time);
-
     // Parse the body
     const data = JSON.parse(event.body || "{}");
     const recipe_name = data.recipe_name;
@@ -57,25 +43,19 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
         };
     }
 
-    const prompt: string = `
+    const system_prompt: string = `
     Craft a lengthy, convoluted story about the recipe named ${recipe_name}. 
-    Dive deep into the myriad details of how you discovered it, details could include a day with peculiar weather, 
-    like a time when the forecast was off. Embellish with over-the-top 
-    descriptions of your surroundings, such as unexpected colors in the sky or a passerby's curious attire. 
-    Recall the roller-coaster of emotions when you first tried preparing it, and perhaps a touch of drama from opinionated relatives, 
-    like a grandparent who believes in a unique method of preparation or a sibling with a strange substitution idea. 
-    Hint at some light-hearted family events related to this dish. Let your imagination roam, 
+    Dive deep into the myriad details of how you discovered it. Let your imagination roam, 
     and craft this tale as if the recipe has woven itself intricately into your life's rich narrative. 
-    And remember, always leave the story open-ended, never drawing it to a full conclusion, as the tale should always have room to grow and expand.
-    Seamlessly continue the story from the following sentences: '${last_sentences}'. 
-    `;
+    And remember, always leave the story open-ended, never drawing it to a full conclusion, as the tale should always have room to grow and expand.`;
 
     try {
         const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
-	    messages: [{"role": "user", "content": prompt}],
-            max_tokens: 1700
+	    messages: [{"role": "system", "content": system_prompt}, {"role": "user", "content": last_sentences}],
+            max_tokens: 200
         });
+        console.log(`Response: ${response.choices[0]["message"]["content"]}`)
 
         return {
             statusCode: 200,
@@ -83,6 +63,7 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
             headers: headers
         };
     } catch (error) {
+        console.log(`Error in OpenAI request: ${error.message}`)
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message }),
